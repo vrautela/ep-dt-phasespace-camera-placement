3458e35d2d2e7391b59862814f4972b48b5cdddc
Vedant Rautela <v.rautela01@gmail.com>
2022-06-23 11:36:41 +0200

    fix bug in objective_function that wasn't looking at all cameras; add tighter upper and lower bounds

--- optimizer.py
+++ optimizer.py
@@ -10,19 +10,19 @@ fov_degree = 50
 fov_base_radius = fov_range * np.tan(np.deg2rad(fov_degree))
 
 # dimensions of V (in m)
-V_x = 5
-V_y = 5
-V_z = 5
+V_x = 8
+V_y = 8
+V_z = 8
 V = (V_x, V_y, V_z)
 
 # number of cameras
-N = 2
+N = 8
 
 # numbers of position variables (and also number of angle variables)
 NUM_VAR = 5
 
 # the scale of the grid
-epsilon = 1
+epsilon = 0.5
 
 
 # return the number of points that lie in the FOV of at least two cameras
@@ -45,11 +45,13 @@ def objective_function(x):
                 fov_count = 0
                 # loop over each camera to see if the point at (p_x, p_y, p_z) lies in its FOV
                 for i in range(N):
-                    cam_x = x[i]
-                    cam_y = x[i+1]
-                    cam_z = x[i+2]
-                    cam_theta = x[i+3]
-                    cam_phi = x[i+4]
+                    # WOAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH THIS IS SO WRONG (HAD TO ADD CAM_START)
+                    cam_start = i * NUM_VAR
+                    cam_x = x[cam_start]
+                    cam_y = x[cam_start+1]
+                    cam_z = x[cam_start+2]
+                    cam_theta = x[cam_start+3]
+                    cam_phi = x[cam_start+4]
 
                     pos_vec = np.array([cam_x, cam_y, cam_z])
                     # compute the unit vector defining the orientation based on the angles in spherical coords
@@ -86,16 +88,16 @@ def in_fov(pos_vec, orientation_vec, grid_point):
     return dist_from_axis < radius_at_cone_dist
 
 
-def random_position_and_orientation():
-    x = np.random.random() * V_x
-    y = np.random.random() * V_y
-    z = np.random.random() * V_z
+# def random_position_and_orientation():
+#     x = np.random.random() * V_x
+#     y = np.random.random() * V_y
+#     z = np.random.random() * V_z
     
-    # generate spherical coordinate angles (in radians) to define the orientation of the FOV
-    theta = np.deg2rad(np.random.random() * 180)
-    phi = np.deg2rad(np.random.random() * 360)
+#     # generate spherical coordinate angles (in radians) to define the orientation of the FOV
+#     theta = np.deg2rad(np.random.random() * 180)
+#     phi = np.deg2rad(np.random.random() * 360)
 
-    return [x, y, z, theta, phi]
+#     return [x, y, z, theta, phi]
 
 
 if __name__ == '__main__':
@@ -114,21 +116,38 @@ if __name__ == '__main__':
     # the positions can't be less than 0, and the unit vec components can't be less than -1
     # lower_bounds = [0, 0, 0, 0, 0] * N
     # the positions can't be greater than the dimensions of V, and the unit vec components can't be greater than 1
-    upper_bounds = [V_x, V_y, V_z, 180, 360] * N
-    bounds = Bounds(0, upper_bounds)
+    # upper_bounds = [V_x, V_y, V_z, np.deg2rad(180), np.deg2rad(360)] * N
+    lower_bounds = [
+        0, 0, 0, np.deg2rad(0), np.deg2rad(0),
+        0, 0, 0, np.deg2rad(90), np.deg2rad(0),
+        0, 0, 0, np.deg2rad(0), np.deg2rad(270),
+        0, 0, 0, np.deg2rad(90), np.deg2rad(270),
+        0, 0, 0, np.deg2rad(0), np.deg2rad(90),
+        0, 0, 0, np.deg2rad(90), np.deg2rad(90),
+        0, 0, 0, np.deg2rad(0), np.deg2rad(180),
+        0, 0, 0, np.deg2rad(90), np.deg2rad(180),
+    ]
+    upper_bounds = [
+        V_x, V_y, V_z, np.deg2rad(90), np.deg2rad(90),
+        V_x, V_y, V_z, np.deg2rad(180), np.deg2rad(90),
+        V_x, V_y, V_z, np.deg2rad(90), np.deg2rad(360),
+        V_x, V_y, V_z, np.deg2rad(180), np.deg2rad(360),
+        V_x, V_y, V_z, np.deg2rad(90), np.deg2rad(180),
+        V_x, V_y, V_z, np.deg2rad(180), np.deg2rad(180),
+        V_x, V_y, V_z, np.deg2rad(90), np.deg2rad(270),
+        V_x, V_y, V_z, np.deg2rad(180), np.deg2rad(270),
+    ]
+    bounds = Bounds(lower_bounds, upper_bounds)
 
-    # provide an initial guess of the cameras' positions
-    # TODO: change this so that each variable is chosen randomly from between the appropriate bounds
-    x0 = []
-    for i in range(N):
-        po = random_position_and_orientation()
-        print(po)
-        x0.extend(po)
+    from guesses import gen_guess_cube
     
+    x0 = gen_guess_cube(V_x)
+
     print("minimizing")
     # res = minimize(objective_function, x0, bounds=bounds)
-    res = minimize(objective_function, x0, bounds=bounds, options={"eps":1, "disp":True})
+    res = minimize(objective_function, x0, bounds=bounds, options={"eps":0.1, "disp":True})
     print(res)
 
-    solution = res.x
-    print(solution)
+    f_value = res.fun
+    num_points = int(V_x / epsilon) * int(V_y / epsilon) * int(V_z / epsilon)
+    print(str(-f_value) + " out of " + str(num_points))
