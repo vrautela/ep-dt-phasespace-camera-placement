fix cylinder intersection computation and change directory structure

optimization/obstacles.py
@@ -8,12 +8,17 @@ class Obstacle:
    def does_line_segment_intersect(self, p1: np.ndarray, p2: np.ndarray) -> bool:
       raise NotImplementedError(f"Class {self.__class__.__name__} doesn't implement does_line_segment_intersect()")
 
+   # Add this obstacle to the given matplotlib figure
+   def add_to_plot(self, fig, ax):
+      raise NotImplementedError(f"Class {self.__class__.__name__} doesn't implement add_to_plot()")
+      
+
 
 # class representing a cylindrical obstacle
 class CylinderObstacle(Obstacle):
    """Class representing a cylindrical obstacle"""
 
-   def __init__(self, A, B, R):
+   def __init__(self, A: np.ndarray, B: np.ndarray, R):
       # A and B are the centers of the bases of the cylinder
       # R is the radius of the cylinder
       # h is the height of the cylinder
@@ -105,7 +110,7 @@ class CylinderObstacle(Obstacle):
       return 0 < z_int_1 < self.h or 0 < z_int_2 < self.h
 
 
-   def rotation_matrix_align_two_vecs(a: np.ndarray, b: np.ndarray):
+   def rotation_matrix_align_two_vecs(self, a: np.ndarray, b: np.ndarray):
       """
       R = I + [v]_x + (1/1+c)([v]_x)^2 
       """
@@ -113,9 +118,42 @@ class CylinderObstacle(Obstacle):
       c = np.dot(a, b)
 
       v1, v2, v3 = v[0], v[1], v[2]
-      v_ss_cp = np.array([0,-v3,v2], [v3,0,-v1], [-v2,v1,0])
+      v_ss_cp = np.array([[0,-v3,v2], [v3,0,-v1], [-v2,v1,0]])
       
       I = np.eye(3)
       R = I + v_ss_cp + (1/(1+c))*np.linalg.matrix_power(v_ss_cp, 2)
 
       return R
+   
+   def add_to_plot(self, fig, ax):
+      origin = np.array([0, 0, 0])
+      #axis and radius
+      p0 = self.A
+      p1 = self.B
+      R = self.R
+      #vector in direction of axis
+      v = p1 - p0
+      #find magnitude of vector
+      mag = np.linalg.norm(v)
+      #unit vector in direction of axis
+      v = v / mag
+      #make some vector not in the same direction as v
+      not_v = np.array([1, 0, 0])
+      if (v == not_v).all():
+         not_v = np.array([0, 1, 0])
+      #make vector perpendicular to v
+      n1 = np.cross(v, not_v)
+      #normalize n1
+      n1 /= np.linalg.norm(n1)
+      #make unit vector perpendicular to v and n1
+      n2 = np.cross(v, n1)
+      #surface ranges over t from 0 to length of axis and 0 to 2*pi
+      t = np.linspace(0, mag, 100)
+      theta = np.linspace(0, 2 * np.pi, 100)
+      #use meshgrid to make 2d arrays
+      t, theta = np.meshgrid(t, theta)
+      #generate coordinates for surface
+      X, Y, Z = [p0[i] + v[i] * t + R * np.sin(theta) * n1[i] + R * np.cos(theta) * n2[i] for i in [0, 1, 2]]
+      ax.plot_surface(X, Y, Z)
+      #plot axis
+      ax.plot(*zip(p0, p1), color = 'red')

optimization/optimizer.py
@@ -1,8 +1,11 @@
-from guesses import gen_guess_box
 import math
 from math import sqrt
 import numpy as np
 from scipy.optimize import Bounds, minimize, NonlinearConstraint
+from typing import List
+
+from guesses import gen_guess_box
+from obstacles import CylinderObstacle, Obstacle
 
 # TODO: change these to reflect the most accurate specs
 # FOV lower bound (objects must be at least this far to be seen)
@@ -38,7 +41,7 @@ def grid_dimensions(V_x, V_y, V_z):
     return n_x, n_y, n_z
 
 
-def average_reciprocal_gdop(x):
+def average_reciprocal_gdop(x, obstacles: List[Obstacle]):
     total = 0
 
     # loop over all points in the grid defined by cutting V every epsilon meters
@@ -71,29 +74,38 @@ def average_reciprocal_gdop(x):
                     orientation_z = np.cos(cam_theta)
                     orientation_vec = np.array([orientation_x, orientation_y, orientation_z])
 
+                    # check if this camera can see the point
                     if in_fov(pos_vec, orientation_vec, grid_point):
-                        # TODO: change this so the position of the sensors inside the camera is accurate
-                        # Each camera has two sensors inside
-                        sensor_sep = 0.01
-                        reachable_cams.extend([cam_x + sensor_sep, cam_y, cam_z])
-                        reachable_cams.extend([cam_x - sensor_sep, cam_y, cam_z])
+                        obscured = False
+                        # check if any of the obstacles obscures the camera's vision
+                        for ob in obstacles:
+                            if ob.does_line_segment_intersect(pos_vec, grid_point):
+                                obscured = True
+                                break
+                        
+                        if not obscured:
+                            # TODO: change this so the position of the sensors inside the camera is accurate
+                            # Each camera has two sensors inside
+                            sensor_sep = 0.01
+                            reachable_cams.extend([cam_x + sensor_sep, cam_y, cam_z])
+                            reachable_cams.extend([cam_x - sensor_sep, cam_y, cam_z])
 
-                        NUM_POS_VARS = 3
-                        MIN_CAMS_VISIBLE = 6
-                        if not triangulable and len(reachable_cams) >= MIN_CAMS_VISIBLE * NUM_POS_VARS:
-                            # check triangulability of this camera with all the other reachable ones
-                            for cam in triangulable_cams:
-                                comp_pos_vec, comp_orientation_vec = cam[0], cam[1]
-                                # get vector from point to pos and point to comp_pos
-                                point_to_pos = np.subtract(pos_vec, grid_point)
-                                point_to_comp_pos = np.subtract(comp_pos_vec, grid_point)
-                                # compute angle between the two pos vectors (in degrees) and see if triangulable
-                                alpha = angle_between(point_to_pos, point_to_comp_pos)
+                            NUM_POS_VARS = 3
+                            MIN_CAMS_VISIBLE = 6
+                            if not triangulable and len(reachable_cams) >= MIN_CAMS_VISIBLE * NUM_POS_VARS:
+                                # check triangulability of this camera with all the other reachable ones
+                                for cam in triangulable_cams:
+                                    comp_pos_vec, comp_orientation_vec = cam[0], cam[1]
+                                    # get vector from point to pos and point to comp_pos
+                                    point_to_pos = np.subtract(pos_vec, grid_point)
+                                    point_to_comp_pos = np.subtract(comp_pos_vec, grid_point)
+                                    # compute angle between the two pos vectors (in degrees) and see if triangulable
+                                    alpha = angle_between(point_to_pos, point_to_comp_pos)
 
-                                # check if the angle between the two cameras falls between the triangulability bounds  
-                                if 40 < alpha and alpha < 140:
-                                    triangulable = True
-                            triangulable_cams.append((pos_vec, orientation_vec))
+                                    # check if the angle between the two cameras falls between the triangulability bounds  
+                                    if 40 < alpha and alpha < 140:
+                                        triangulable = True
+                                triangulable_cams.append((pos_vec, orientation_vec))
 
                 if triangulable:
                     g = gdop(reachable_cams, grid_point)
@@ -338,7 +350,7 @@ def main():
     x0 = gen_guess_box(V_x, V_y, V_z)
 
     # specify the obstacles present in the space
-    obstacles = []
+    obstacles = [CylinderObstacle(np.array([1.5,6,1.5]), np.array([2.5,6,1.5]), 0.25)]
 
     res = minimize(average_reciprocal_gdop, x0, args=(obstacles,), bounds=bounds, options={"eps":0.1, "disp":True})
     print(res)

test/test_gdop_triangulability.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from math import sqrt
 import numpy as np
-from optimizer import angle_between, sigmoid
+from optimal_camera_placement.optimization.optimizer import angle_between, sigmoid
 from scipy.optimize import Bounds, minimize
 # Test of GDOP vs triangulability metrics
 